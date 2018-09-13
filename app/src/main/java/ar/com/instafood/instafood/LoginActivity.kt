83241676fdfc0c94cca5c25394d4fece776d86b9
Flagohuvelle <leandrogabrielgarcia@gmail.com>
2018-09-13 16:10:30 -0300

    Primer Commit - Login con FireBase Authentication y FireStore

---
+++ app/src/main/java/ar/com/instafood/instafood/LoginActivity.kt
@@ -0,0 +1,67 @@
+package ar.com.instafood.instafood
+
+import android.content.Intent
+import android.graphics.Color
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var txtUser: EditText
+    private lateinit var txtPassword: EditText
+    private lateinit var progressBar: ProgressBar
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        getWindow().getDecorView().setBackgroundColor(Color.WHITE)
+        txtUser = findViewById(R.id.txtUser)
+        txtPassword = findViewById(R.id.txtPassword)
+        progressBar = findViewById(R.id.progressBar)
+        auth = FirebaseAuth.getInstance()
+    }
+
+    fun forgotPassword(view: View){
+        startActivity(Intent(this,ForgotPass::class.java))
+    }
+
+    fun register(view: View){
+        startActivity(Intent(this,RegisterActivity::class.java))
+    }
+
+    fun login(view: View){
+        loginUser()
+    }
+
+    private fun loginUser(){
+        val user:String = txtUser.text.toString()
+        val password:String = txtPassword.text.toString()
+
+        if (!TextUtils.isEmpty(user) && !TextUtils.isEmpty(password)){
+            progressBar.visibility=View.VISIBLE
+
+            auth.signInWithEmailAndPassword(user,password)
+                .addOnCompleteListener(this){
+                    task ->
+                    if(task.isSuccessful){
+                        action()
+                    }
+                    else{
+                        progressBar.visibility=View.GONE
+                        Toast.makeText(this,"Error al logear", Toast.LENGTH_LONG).show()
+                    }
+                }
+        }
+    }
+
+    private fun action(){
+        startActivity(Intent(this,MainActivity::class.java))
+    }
+}
