bb67d5caa34e776afa666d9cd18dd51a9a5751d3
Fabian Cardaci <fabiancardaci@gmail.com>
2018-10-01 00:40:14 -0300

    Initial Close table

--- app/src/main/java/ar/com/instafood/fragments/OrderFragment.kt
+++ app/src/main/java/ar/com/instafood/fragments/OrderFragment.kt
@@ -1,29 +1,37 @@
 package ar.com.instafood.fragments
 
-
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import ar.com.instafood.activities.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- *
- */
 class OrderFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_order, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        switchSearchRestaurants()
+    }
+
+    private fun switchSearchRestaurants() {
+        val restSearch = getView()?.findViewById<Button>(R.id.btn_close_table)
+        restSearch?.setOnClickListener { _ ->
+            val amount = view?.findViewById<TextView>(R.id.txt_final_amount)
+            val amountFormat = this.resources.getString(R.string.final_amount)
+            amount?.text = amountFormat + "808.30"
+        }
+    }
+
 
 
 }
