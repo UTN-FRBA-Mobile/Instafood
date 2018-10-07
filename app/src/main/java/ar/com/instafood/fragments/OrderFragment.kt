521f67b8f26e955ae28798cbccc18b9876407bac
Fabian Cardaci <fabiancardaci@gmail.com>
2018-10-07 02:49:52 -0300

    Timer - first commit

--- app/src/main/java/ar/com/instafood/fragments/OrderFragment.kt
+++ app/src/main/java/ar/com/instafood/fragments/OrderFragment.kt
@@ -1,28 +1,155 @@
 package ar.com.instafood.fragments
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.support.v4.app.Fragment
+import android.support.v7.widget.Toolbar
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
 import ar.com.instafood.activities.R
-
+import ar.com.instafood.fragments.order.PrefUtil
+import kotlinx.android.synthetic.main.fragment_order.*
 
 class OrderFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater,
-                              container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+
+    enum class TimerState{
+        Stopped, Paused, Running
+    }
+
+    private lateinit var timer: CountDownTimer
+    private var timerLengthSeconds: Long = 0
+    private var timerState = TimerState.Stopped
+
+    private var secondsRemaining: Long = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        initTimer()
+        //TODO: hide notification
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        if (timerState == TimerState.Running){
+            timer.cancel()
+            //TODO: show notification
+        }
+        else if (timerState == TimerState.Paused){
+            //TODO: show notification
+        }
+
+        PrefUtil.setPreviousTimerLengthSeconds(timerLengthSeconds, this.requireContext())
+        PrefUtil.setSecondsRemaining(secondsRemaining, this.requireContext())
+        PrefUtil.setTimerState(timerState, this.requireContext())
+    }
+
+    private fun initTimer(){
+        timerState = PrefUtil.getTimerState(this.requireContext())
+
+        //we don't want to change the length of the timer which is already running
+        //if the length was changed in settings while it was backgrounded
+        if (timerState == TimerState.Stopped)
+            setNewTimerLength()
+        else
+            setPreviousTimerLength()
+
+        secondsRemaining = if (timerState == TimerState.Running || timerState == TimerState.Paused)
+            PrefUtil.getSecondsRemaining(this.requireContext())
+        else
+            timerLengthSeconds
+
+        if (secondsRemaining <= 0)
+            onTimerFinished()
+        else if (timerState == TimerState.Running)
+            startTimer()
+
+        updateCountdownUI()
+    }
+
+    private fun onTimerFinished(){
+        timerState = TimerState.Stopped
+
+        //set the length of the timer to be the one set in SettingsActivity
+        //if the length was changed when the timer was running
+        setNewTimerLength()
+
+        progress_countdown.progress = 0
+
+        PrefUtil.setSecondsRemaining(timerLengthSeconds, this.requireContext())
+        secondsRemaining = timerLengthSeconds
+
+        updateCountdownUI()
+    }
+
+    private fun startTimer(){
+        timerState = TimerState.Running
+
+        timer = object : CountDownTimer(secondsRemaining * 1000, 1000) {
+            override fun onFinish() = onTimerFinished()
+
+            override fun onTick(millisUntilFinished: Long) {
+                secondsRemaining = millisUntilFinished / 1000
+                updateCountdownUI()
+            }
+        }.start()
+    }
+
+    private fun setNewTimerLength(){
+        val lengthInMinutes = PrefUtil.getTimerLength(this.requireContext())
+        timerLengthSeconds = (lengthInMinutes * 60L)
+        progress_countdown.max = timerLengthSeconds.toInt()
+    }
+
+    private fun setPreviousTimerLength(){
+        timerLengthSeconds = PrefUtil.getPreviousTimerLengthSeconds(this.requireContext())
+        progress_countdown.max = timerLengthSeconds.toInt()
+    }
+
+    private fun updateCountdownUI(){
+        val minutesUntilFinished = secondsRemaining / 60
+        val secondsInMinuteUntilFinished = secondsRemaining - minutesUntilFinished * 60
+        val secondsStr = secondsInMinuteUntilFinished.toString()
+        txt_countdown.text = "$minutesUntilFinished:${if (secondsStr.length == 2) secondsStr else "0" + secondsStr}"
+        progress_countdown.progress = (timerLengthSeconds - secondsRemaining).toInt()
+    }
+
+
+
+
+
+
+
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_order, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        switchSearchRestaurants()
+        initToolbar()
+
+        fab_start.setOnClickListener{v ->
+            startTimer()
+            timerState =  TimerState.Running
+        }
     }
 
+    private fun initToolbar() {
+        val toolbar = view?.findViewById<Toolbar>(R.id.toolbar)
+        toolbar?.setNavigationIcon(R.drawable.ic_restaurant)
+        toolbar?.title = "En Mesa"
+    }
+
+
+/*
     private fun switchSearchRestaurants() {
         val restSearch = getView()?.findViewById<Button>(R.id.btn_close_table)
         restSearch?.setOnClickListener { _ ->
@@ -31,7 +158,6 @@ class OrderFragment : Fragment() {
             amount?.text = amountFormat + "808.30"
         }
     }
-
-
+*/
 
 }
