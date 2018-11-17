b8f08ea6e7e99462305b3c800f7a1d05fcb42485
Matias Navarro <mnavarro@MacBook-Pro.local>
2018-11-16 21:31:37 -0300

    agregado de metodos para guardar la imagen

--- app/src/main/java/ar/com/instafood/activities/ScanActivity.kt
+++ app/src/main/java/ar/com/instafood/activities/ScanActivity.kt
@@ -10,9 +10,19 @@ import ar.com.instafood.application.SocketApplication
 import com.google.zxing.Result
 import me.dm7.barcodescanner.zxing.ZXingScannerView
 import ar.com.instafood.fragments.MenuFragment
-import ar.com.instafood.models.Check
-import ar.com.instafood.models.Product
-import com.google.gson.GsonBuilder
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.MultiFormatWriter
+import com.google.zxing.WriterException
+import com.google.zxing.common.BitMatrix
+import android.graphics.Bitmap
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import android.os.Environment
+import android.media.MediaScannerConnection
+import java.util.Calendar
+import android.widget.ImageView
 
 /**
  * Created by mnavarro on 24/10/2018.
@@ -20,11 +30,14 @@ import com.google.gson.GsonBuilder
 class ScanActivity : AppCompatActivity(), ZXingScannerView.ResultHandler {
     private var mScannerView: ZXingScannerView? = null
     private var menuFragment = MenuFragment()
+    internal var bitmap: Bitmap? = null
+    private var iv: ImageView? = null
 
     public override fun onCreate(state: Bundle?) {
         super.onCreate(state)
         mScannerView = ZXingScannerView(this)   // Programmatically initialize the scanner view
         setContentView(mScannerView)                // Set the scanner view as the content view
+        convertToImage("lo que sea");
     }
 
     public override fun onResume() {
@@ -51,6 +64,91 @@ class ScanActivity : AppCompatActivity(), ZXingScannerView.ResultHandler {
 
         socket?.emit("restaurantSelected", resultFromScanner)
     }
+
+    private fun convertToImage(text: String){
+            bitmap = TextToImageEncode(text)
+            iv!!.setImageBitmap(bitmap)
+
+    }
+    fun saveImage(myBitmap: Bitmap?): String {
+        val bytes = ByteArrayOutputStream()
+        myBitmap!!.compress(Bitmap.CompressFormat.JPEG, 90, bytes)
+        val wallpaperDirectory = File(
+                Environment.getExternalStorageDirectory().toString() + IMAGE_DIRECTORY)
+        // have the object build the directory structure, if needed.
+
+        if (!wallpaperDirectory.exists()) {
+            Log.d("dirrrrr", "" + wallpaperDirectory.mkdirs())
+            wallpaperDirectory.mkdirs()
+        }
+
+        try {
+            val f = File(wallpaperDirectory, Calendar.getInstance()
+                    .timeInMillis.toString() + ".jpg")
+            f.createNewFile()   //give read write permission
+            val fo = FileOutputStream(f)
+            fo.write(bytes.toByteArray())
+            MediaScannerConnection.scanFile(this,
+                    arrayOf(f.path),
+                    arrayOf("image/jpeg"), null)
+            fo.close()
+            Log.d("TAG", "File Saved::--->" + f.absolutePath)
+
+            return f.absolutePath
+        } catch (e1: IOException) {
+            e1.printStackTrace()
+        }
+
+        return ""
+
+    }
+
+    @Throws(WriterException::class)
+    private fun TextToImageEncode(Value: String): Bitmap? {
+        val bitMatrix: BitMatrix
+        try {
+            bitMatrix = MultiFormatWriter().encode(
+                    Value,
+                    BarcodeFormat.QR_CODE,
+                    QRcodeWidth, QRcodeWidth, null
+            )
+
+        } catch (Illegalargumentexception: IllegalArgumentException) {
+
+            return null
+        }
+
+        val bitMatrixWidth = bitMatrix.getWidth()
+
+        val bitMatrixHeight = bitMatrix.getHeight()
+
+        val pixels = IntArray(bitMatrixWidth * bitMatrixHeight)
+
+        for (y in 0 until bitMatrixHeight) {
+            val offset = y * bitMatrixWidth
+
+            for (x in 0 until bitMatrixWidth) {
+
+                pixels[offset + x] = if (bitMatrix.get(x, y))
+                    resources.getColor(R.color.abc_hint_foreground_material_dark)
+                else
+                    resources.getColor(R.color.white)
+            }
+        }
+        val bitmap = Bitmap.createBitmap(bitMatrixWidth, bitMatrixHeight, Bitmap.Config.ARGB_4444)
+
+        bitmap.setPixels(pixels, 0, 500, 0, 0, bitMatrixWidth, bitMatrixHeight)
+        return bitmap
+    }
+
+    companion object {
+
+        val QRcodeWidth = 500
+
+        private val IMAGE_DIRECTORY = "/QRcodeDemonuts"
+    }
+
+
 
     private fun returnMain(text : String){
         /**
