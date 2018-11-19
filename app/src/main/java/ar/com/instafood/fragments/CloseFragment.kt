d9d4ee580538dd7eaf0a43683e0a83ac0ec6b200
Fabian Cardaci <fabiancardaci@gmail.com>
2018-11-19 19:45:26 -0300

    Toast

--- app/src/main/java/ar/com/instafood/fragments/CloseFragment.kt
+++ app/src/main/java/ar/com/instafood/fragments/CloseFragment.kt
@@ -5,8 +5,13 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import ar.com.instafood.activities.R
 import ar.com.instafood.fragments.order.StarService
+import android.widget.Toast
+import android.R.attr.data
+
+
 
 class CloseFragment : Fragment() {
 
@@ -27,6 +32,11 @@ class CloseFragment : Fragment() {
         val starService = StarService()
         starService.init(this.view!!, this.requireContext())
 
+        val closeButton = view?.findViewById<Button>(R.id.btn_close_score)
+        closeButton?.setOnClickListener { _ ->
+            Toast.makeText(getActivity(), "This is my Toast message!", Toast.LENGTH_LONG).show();
+            closeButton.visibility = View.INVISIBLE
+        }
     }
 
 
