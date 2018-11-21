972910f2d421f7703fc2129d87c6a43156721062
Flagohuvelle <leandrogabrielgarcia@gmail.com>
2018-11-21 20:38:44 -0300

    fix layour menu y validation para q no rompa cuando no hay ningun producto agregado

--- app/src/main/java/ar/com/instafood/fragments/CheckFragment.kt
+++ app/src/main/java/ar/com/instafood/fragments/CheckFragment.kt
@@ -40,6 +40,7 @@ class CheckFragment : Fragment() {
         app.socket?.on("products", { args -> run {
             activity?.runOnUiThread(
                     {
+                    if (args[0] != null ) {
                         val jsonElement = JsonParser().parse((args[0] as JSONObject).toString())
                         checks = arrayListOf(Gson().fromJson(jsonElement, Check::class.java))
                         var mDynamicListAdapter = DynamicCheckAdapter()
@@ -47,6 +48,7 @@ class CheckFragment : Fragment() {
                         view.cardList.setHasFixedSize(true)
                         view.cardList.layoutManager = LinearLayoutManager(context)
                         view.cardList.adapter = mDynamicListAdapter
+                    }
                     }
             )
 
