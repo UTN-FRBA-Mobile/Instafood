b1d74d1dd265e1b738018920901b4e10505cdb65
Flagohuvelle <leandrogabrielgarcia@gmail.com>
2018-11-21 21:53:43 -0300

    Introduciendo el login al agregar un pedido, y metiendo el user en una variable estatica, para armar los pedidos

--- app/src/main/java/ar/com/instafood/activities/login/LoginActivity.kt
+++ app/src/main/java/ar/com/instafood/activities/login/LoginActivity.kt
@@ -3,6 +3,7 @@ package ar.com.instafood.activities.login
 import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import android.text.TextUtils
 import android.view.View
@@ -11,7 +12,10 @@ import android.widget.ProgressBar
 import android.widget.Toast
 import ar.com.instafood.activities.MainActivity
 import ar.com.instafood.activities.R
+import ar.com.instafood.fragments.MenuFragment
+import ar.com.instafood.fragments.menuFragments.ProductDetailFragment
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_menu.*
 
 class LoginActivity : AppCompatActivity() {
 
@@ -53,7 +57,9 @@ class LoginActivity : AppCompatActivity() {
                 .addOnCompleteListener(this){
                     task ->
                     if(task.isSuccessful){
-                        action()
+                        ProductDetailFragment.userName = user
+                        this.finish()
+                        //action()
                     }
                     else{
                         progressBar.visibility=View.GONE
@@ -64,6 +70,13 @@ class LoginActivity : AppCompatActivity() {
     }
 
     private fun action(){
-        startActivity(Intent(this, MainActivity::class.java))
+
+        val fragment = fragmentManager!!.findFragmentByTag("MENU_FRAGMENT") as MenuFragment
+        //val transaction = fragmentManager!!.beginTransaction()
+        //transaction.replace(R.id.fragment_container, fragment, "MENU_FRAGMENT");
+        //transaction.addToBackStack(null);
+        //transaction.commit();
+        startActivityFromFragment(fragment, Intent(this, MainActivity::class.java),1)
+        //startActivity(Intent(this, MainActivity::class.java))
     }
 }
